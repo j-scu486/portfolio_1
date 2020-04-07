a1b3b2999de9dd0a95d3f462b074d7d32a07a8c1
thesaladbowl <nocturne486@gmail.com>
2020-04-07 20:20:01 +0900

    changes

--- script.js
+++ script.js
@@ -30,16 +30,46 @@ gridContainer.addEventListener("click", e => {
     openModal(modal);
 });
 
-// Navbar-shrink scroll effect
+// GSAP
+var contactSection = document.querySelector("#contact");
+var tl = gsap.timeline()
+var tl2 = new TimelineMax({repeat:-1, yoyo:true});
+var tl3 = gsap.timeline({paused: true})
 
-window.onscroll = function() {scrollFunction()};
+tl.to(".showcase-title", {duration: 2, opacity: 1})
+  .to(".web-dev-header", {duration: 2, opacity: 1}, "=-1")
+  .to(".snowball-1", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 1)
+  .to(".snowball-2", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 5)
+  .to(".snowball-3", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 4)
+  .to(".snowball-4", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 3)
+  .to(".snowball-5", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 2)
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.querySelector(".navbar-links").style.fontSize = "1rem";
-    document.querySelector(".navbar-logo").style.fontSize = "1.5rem";
-  } else {
-    document.querySelector(".navbar-links").style.fontSize = "1.25rem";
-    document.querySelector(".navbar-logo").style.fontSize = "2rem";
+tl2.to(".cloud-1", {duration: 20, x:100})
+   .to(".cloud-2", {duration: 20, x:-100}, 0 )
+
+tl3.from(".citrus-1", {duration: 2, y: -500, ease: "bounce"}, 0)
+   .from(".citrus-2", {duration: 2, y: -1000, ease: "bounce"}, 0.2)
+   .from(".citrus-3", {duration: 2, y: -1000, ease: "bounce"}, 0.25)
+
+window.addEventListener("scroll", function(){
+  if (window.scrollY > (contactSection.offsetTop)) {
+    tl3.play()
   }
-}
+})
+
+// Navbar display on scroll
+var navBar = document.querySelector(".navbar-container");
+var aboutMeSection = document.querySelector("#what .container");
+
+window.addEventListener("scroll", function(){
+  if (window.scrollY > (aboutMeSection.offsetHeight)) {
+    navBar.style.transform = "translateX(0)";
+  } else {
+    navBar.style.transform = "translateX(-100px)";
+  }
+})
+
+// Snowball hover effect
+
+var snowball1 = document.querySelector(".snowball-1");
+
