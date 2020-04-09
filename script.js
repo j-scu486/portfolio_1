64e667918a4f9bfca47737678ce931d389617387
thesaladbowl <nocturne486@gmail.com>
2020-04-09 19:08:41 +0900

    loading

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Loading overlay
-
-var bodySelector = document.querySelector("body")
 
 window.onload = function(){
   bodySelector.style.visibility = "visible"
@@ -39,33 +36,46 @@ gridContainer.addEventListener("click", e => {
 });
 
 // GSAP
-var contactSection = document.querySelector("#contact");
-var tl = gsap.timeline()
-var tl2 = new TimelineMax({repeat:-1, yoyo:true});
-var tl3 = gsap.timeline({paused: true})
 
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", function() {
+  window.onload = function() {
+    // Wait until page is loaded before animations
+    
+    var bodySelector = document.querySelector("body")
+    bodySelector.style.visibility = "visible"
+
+    var contactSection = document.querySelector("#contact");
+    var tl = gsap.timeline()
+    var tl2 = new TimelineMax({repeat:-1, yoyo:true});
+    var tl3 = gsap.timeline({paused: true})
+
     tl.to(".showcase-title", {duration: 2, opacity: 1})
-    .to(".web-dev-header", {duration: 2, opacity: 1}, "=-1")
-    .to(".snowball-1", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 1)
-    .to(".snowball-2", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 5)
-    .to(".snowball-3", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 4)
-    .to(".snowball-4", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 3)
-    .to(".snowball-5", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 2)
+      .to(".web-dev-header", {duration: 2, opacity: 1}, "=-1")
+      .to(".snowball-1", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 1)
+      .to(".snowball-2", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 5)
+      .to(".snowball-3", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 4)
+      .to(".snowball-4", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 3)
+      .to(".snowball-5", {duration: 7, rotation:90, y: 1000, opacity: 0, repeat: -1}, 2)
 
-  tl2.to(".cloud-1", {duration: 20, x:100})
-    .to(".cloud-2", {duration: 20, x:-100}, 0 )
+    tl2.to(".cloud-1", {duration: 20, x:100})
+      .to(".cloud-2", {duration: 20, x:-100}, 0 )
 
-  tl3.from(".citrus-1", {duration: 2, y: -500, ease: "bounce"}, 0)
-    .from(".citrus-2", {duration: 2, y: -1000, ease: "bounce"}, 0.2)
-    .from(".citrus-3", {duration: 2, y: -1000, ease: "bounce"}, 0.25)
+    tl3.from(".citrus-1", {duration: 2, y: -500, ease: "bounce"}, 0)
+      .from(".citrus-2", {duration: 2, y: -1000, ease: "bounce"}, 0.2)
+      .from(".citrus-3", {duration: 2, y: -1000, ease: "bounce"}, 0.25)
 
-  window.addEventListener("scroll", function(){
-    if (window.scrollY > (contactSection.offsetTop)) {
-      tl3.play()
-    }
-  })
-}
+    window.addEventListener("scroll", function(){
+      if (window.scrollY > (contactSection.offsetTop)) {
+        tl3.play()
+  }
+})
+
+
+  }
+
+})
+
+
 
 
 // Navbar display on scroll
